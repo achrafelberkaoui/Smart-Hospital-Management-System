db99d35171d5d7899d11773a3b4b8d2c54b9b5ef
achrafelberkaoui <achrafelberkoaui@gmail.com>
2026-02-06 14:59:44 +0100

    Ajout de la migration et des seeders pour les utilisateurs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +17,26 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       User::create([
+        'name' => 'achraf',
+        'email' => 'achraf@gmail.com',
+        'password' => Hash::make('achraf123'),
+        'role' => 'admin',
+        'date_naissance' => '2001\10\05'
+       ]);
+       User::create([
+        'name' => 'hamza',
+        'email' => 'hamza@gmail.com',
+        'password' => Hash::make('hamza123'),
+        'role' => 'medecin',
+        'date_naissance' => '2001\10\05'
+       ]);
+       User::create([
+        'name' => 'simo',
+        'email' => 'simo@gmail.com',
+        'password' => Hash::make('simo123'),
+        'role' => 'infirmier',
+        'date_naissance' => '2001\10\05'
+       ]);
     }
 }
